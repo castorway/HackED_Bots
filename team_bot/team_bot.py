533f0a67292e3f5cca396bdffbfeea2fc9cfb89c
Shem <castorsh3m@gmail.com>
2023-11-12 21:56:39 +0000

    fixed up teambot, use colour instead of internal directory of roles

--- team_bot/team_bot.py
+++ team_bot/team_bot.py
@@ -17,21 +17,24 @@ import logging
 from pathlib import Path
 from datetime import datetime
 import atexit
+from utils import *
 
-# get path
-file_path = Path(os.path.realpath(__file__)).parents[0]
-
-# load env vars like token
-load_dotenv()
+file_path = Path(os.path.realpath(__file__)).parents[0] # get path
+load_dotenv() # load env vars, like token
 
 # load config from json
 with open(file_path / 'config.json', 'r') as f:
     config = json.load(f)
+
+# additions
+config['team_role_colour_obj'] = discord.Colour.from_str(config['team_role_colour'])
+config_checks(config)
+
 print(config)
 
 # logging setup
 log_handler = logging.FileHandler(filename=file_path / 'team_bot.log', encoding='utf-8', mode='w')
-logging.basicConfig(filename=file_path / 'team_bot_script.log',
+logging.basicConfig(filename=file_path / 'team_bot.log',
                     filemode='w',
                     level=logging.DEBUG)
 
@@ -40,63 +43,22 @@ class Bot(commands.Bot):
     def __init__(self, *argv, **kwargv):
         super().__init__(*argv, **kwargv)
 
-        # load teams; teams dict should map team_name to dict of team info
-        self.teams_path = file_path / "teams.json"
-
-        try:
-            # load the teams.json file
-            with open(self.teams_path, "r") as f:
-                self.teams = json.loads(f)
-        except (FileNotFoundError, TypeError) as e:
-            # warn and create a new empty teams dict
-            logging.warning(e)
-            logging.info("Creating an empty teams dict.")
-            self.teams = {}
-
-        logging.info(f"Loaded teams: {list(self.teams.keys())}")
-
-    def add_team(self, team_id: int, team_dict: dict):
-        ''' Add a team to teams dict. '''
-        if team_id in self.teams.keys():
-            logging.warning(f"Tried to add an already-existing team id={team_id} with info={team_dict}.")
-            raise KeyError(f"Tried to add an already-existing team id={team_id} with info={team_dict}.")
-        else:
-            self.teams[team_id] = team_dict
-            print(f"ADD TEAM CALLED {team}")
-
-    def team_exists(self, team_name: str):
-        return team_name in self.teams.keys()
-
-    def recreate_json(self):
-        ''' Recreate teams json and make a backup of the old one. '''
-        try:
-            # if there is an old teams.json, keep as backup
-            if os.path.exists(self.teams_path):
-                bk_path = file_path / 'backup' / f'teams_{datetime.now().strftime("%m-%d_%H-%M-%S")}.json'
-                os.makedirs(bk_path.parent, exist_ok=True)
-                os.rename(self.teams_path, bk_path)
-            
-            # dump data to new teams.json
-            with open(self.teams_path, "w") as f:
-                self.teams = json.dump(self.teams, f)
-            
-            logging.info(f"Recreated teams.json.")
-
-        except Exception as e:
-            logging.warning(f"Recreating teams.json failed with error: {e}")
-
-    def cleanup(self):
-        ''' Save any data. Should be run at end when program exits. '''
-        self.recreate_json() # save teams json
+    def team_exists(self, ctx: discord.ext.commands.Context, team_name: str):
+        for role in ctx.guild.roles:
+            if role.name == team_name:
+                if role.color != config['team_role_colour_obj']:
+                    logging.warning(f"Team role name {team_name} doesn't have team colour.")
+                return True # regardless of colour
 
 
-
-intents = discord.Intents.all() # TODO: fix this
+intents = discord.Intents.all() # TODO: use less permisisons
 bot = Bot(command_prefix=config['prefix'], intents=intents)
+
 
 @bot.event
 async def on_ready():
-    print(f'We have logged in as {bot.user}')
+    print(f'Logged in as {bot.user}')
+
 
 @bot.command(help=f'''Usage: {config['prefix']}team SomeTeamName @teammate1 @teammate2 @teammate3
 Adds all pinged teammates to the team 'SomeTeamName'.''')
@@ -106,38 +68,64 @@ async def team(ctx):
     if ctx.message.channel.id != config['team_create_channel_id']:
         print("Ignoring ~team called outside team-create channel")
         return
+
+    team_name = ctx.message.content.split(' ')[1] # get team name
+    team_members = list(set([member for member in ctx.message.mentions])) # get unique mentions of users
+
+    logging.info(f"team called with team_name {team_name}, team_members {team_members}")
+
+    for member in team_members:
+
+        # ctx.message.mentions already disincludes role mentions, need to disinclude bots
+        if member.bot:
+            await ctx.message.add_reaction("❌")
+            await ctx.reply(f"Your team was not created; you cannot have a bot on your team!")
+            return
+
+        # ensure user not already in team
+        for role in member.roles:
+            if role.color == config['team_role_colour_obj']:
+                await ctx.message.add_reaction("❌")
+                await ctx.reply(f"Your team was not created; at least one member is already in a team.")
+                return
+        
+    # ensure team name not already taken
+    if bot.team_exists(ctx, team_name):
+        await ctx.message.add_reaction("❌")
+        await ctx.reply(f"Your team was not created; there is already a team with the name '{team_name}'.")
+        return
     
     # ensure number of participants under limit
-    team_members = list(set([member for member in ctx.message.mentions]))
     if len(team_members) > config['max_team_participants']:
         await ctx.message.add_reaction("❌")
         await ctx.reply(f"Your team was not created; teams can have a maximum of {config['max_team_participants']} participants.")
+        return
+
+    # check for empty team
+    if len(team_members) == 0:
+        await ctx.message.add_reaction("❌")
+        await ctx.reply(f"Your team was not created; you cannot have an empty team!")
         return
     
     # ensure all participants aren't already in a team
     for member in team_members:
         for role in member.roles:
-            if role.id in bot.teams:
+            if role.color == config['team_role_colour_obj']:
+                logging.info(f"Team {team_name} not created because member {member} already in team.")
                 await ctx.message.add_reaction("❌")
-                await ctx.reply(f"Your team was not created; teams can have a maximum of {config['max_team_participants']} participants.")
+                await ctx.reply(f"Your team was not created; at least one member is already in a team.")
                 return
 
-    team_name = ctx.message.content.split(' ')[1] # get team name
-
-    # ensure team name not already taken
-    if bot.team_exists(team_name):
-        await ctx.message.add_reaction("❌")
-        await ctx.reply(f"Your team was not created; there is already a team with the name '{team_name}'.")
-    
+    # create team category & role
     team_cat = await ctx.guild.create_category(name=team_name) # category to store team text & vc
-    team_role = await ctx.guild.create_role(name=team_name, mentionable=True) # team role
+    team_role = await ctx.guild.create_role(name=team_name, mentionable=True, colour=config['team_role_colour_obj']) # team role
 
     # Privatize category so that only team and some others can view
     await team_cat.set_permissions(ctx.message.guild.default_role, read_messages=False) # @everyone can't view
     await team_cat.set_permissions(team_role, read_messages=True)
     for role_name in ['organizer', 'mentor', 'volunteer', 'sponsor']:
         await team_cat.set_permissions(
-            dget(ctx.message.guild.roles, id=config['roles'][role_name]),
+            dget(ctx.message.guild.roles, id=config['roles'][role_name]), # get role with ID identified in config
             read_messages=True
         )
 
@@ -149,25 +137,35 @@ async def team(ctx):
     for member in team_members:
         await member.add_roles(team_role)
 
-    # Store info in Bot object
-    team_info = {
-        "member_ids": [member.id for member in team_members],
-        "role_id": team_role.id,
-        "category_id": team_cat.id,
-        "text_id": team_text.id,
-        "vc_id": team_vc.id,
-    }
-    bot.add_team(team_name, team_info)
-
     # React in confirmation and send notification in team text channel
     await ctx.message.add_reaction("✅")
     await team_text.send(f'Hey {" ".join([member.mention for member in team_members])}! Here is your team category & channels.')
 
+    logging.info(f"Team created: {team_name}, {[m.name for m in team_members]}, {team_role}")
 
-# register cleanup functions
-def cleanup():
-    logging.info("atexit cleanup handler")
-    bot.cleanup()
-atexit.register(cleanup)
+
+@bot.command(help=f'''Usage: {config['prefix']}teams_info'. Only usable by Organizer role.''')
+async def teams_info(ctx):
+
+    # ignore if user not an organizer
+    for role in ctx.author.roles:
+        if role.id == config['roles']['organizer']:
+            break
+    else:
+        return
+
+    msg = f"```asciidoc\n"
+    msg += f"TEAM INFO\n"
+    msg += f"==========================\n"
+
+    for role in ctx.guild.roles:
+        if role.colour == config['team_role_colour_obj']:
+            msg += f"[{role.name}]\n"
+            msg += "".join([f"* {member.name} <{member.id}>\n" for member in role.members])
+
+    msg += "```"
+
+    await ctx.send(msg)
+    
 
 bot.run(os.environ['TEAMBOT_TOKEN'], log_handler=log_handler)
--- team_bot/utils.py
+++ team_bot/utils.py
@@ -15,3 +15,9 @@ class JudgingRoom():
     def __init__(self):
         room_no = None
         judges = []
+
+
+def config_checks(config):
+    if config['team_role_colour'] == '#000000':
+        print("WARNING: Config team role colour should not be #000000! This is used as the default colour for @everyone.")
+        exit(0)

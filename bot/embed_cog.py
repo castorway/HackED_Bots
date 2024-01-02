41a51feb2b838be57db1b7c808cd892d76085e31
Shem <castorsh3m@gmail.com>
2024-01-01 17:47:52 -0700

    embed messages & embed_f commands for too-long embeds

--- bot/embed_cog.py
+++ bot/embed_cog.py
@@ -61,3 +61,50 @@ class Embed(commands.Cog):
         # edit the message
         await channel.send(embed=embed)
         await ctx.message.add_reaction("✅")
+
+    @commands.command(help=f'''Edits a message with a custom embed. Restricted.
+    
+    Usage: {config['prefix']}embedit_c "some header" "some content..."''')
+    async def embedit_f(self, ctx, channel: discord.TextChannel, message_id: str):
+        if not utils.check_perms(ctx.message.author, config["perms"]["controller"]):
+            logging.info(f"embedit_f: ignoring nonpermitted call by {ctx.message.author.name}")
+            return
+
+        # download the attachment
+        filename = utils.gen_filename("received_embed", "json")
+        with open(filename, "wb") as f:
+            await ctx.message.attachments[0].save(fp=f)
+
+        # embed contents are file
+        with open(filename, "r") as f:
+            contents = f.read()
+        
+        embed = discord.Embed.from_dict(json.loads(contents.strip(), strict=False))
+
+        # edit the message
+        embed_message = await channel.fetch_message(message_id)
+        await embed_message.edit(embed=embed)
+        await ctx.message.add_reaction("✅")
+
+    @commands.command(help=f'''Edits a message with a custom embed. Restricted.
+    
+    Usage: {config['prefix']}embedit_c "some header" "some content..."''')
+    async def embedin_f(self, ctx, channel: discord.TextChannel):
+        if not utils.check_perms(ctx.message.author, config["perms"]["controller"]):
+            logging.info(f"embedin_f: ignoring nonpermitted call by {ctx.message.author.name}")
+            return
+        
+        # download the attachment
+        filename = utils.gen_filename("received_embed", "json")
+        with open(filename, "wb") as f:
+            await ctx.message.attachments[0].save(fp=f)
+
+        # embed contents are file
+        with open(filename, "r") as f:
+            contents = f.read()
+
+        embed = discord.Embed.from_dict(json.loads(contents.strip(), strict=False))
+
+        # edit the message
+        await channel.send(embed=embed)
+        await ctx.message.add_reaction("✅")
